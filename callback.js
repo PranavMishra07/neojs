568b4faf5acbc58291e10e2287112a3ad810705a
PranavMishra07 <pranav072003@gmail.com>
2025-02-06 15:50:18 +0530

    Aaa

--- callback.js
+++ callback.js
@@ -1,9 +1,28 @@
+//A callback fnction is a function passed as an argument to another function,which is then invoked ("Called back")
 let emp = [
     {
         name: "hiresh", age: 90, id: 101, contact: 123
     },
     {
         name: "animesh", age: 88, id: 102
+    },
+    {
+        name: "Himanshu",
+        id: 1,
+        age: 20,
+        designation: "developer"
+    },
+    {
+        name: "Satyam",
+        id: 2,
+        age: 18,
+        designation: "asistant"
+    },
+    {
+        name: "Arpit",
+        id: 3,
+        age: 16,
+        designation: "manager"
     }
 
 ]
@@ -14,6 +33,6 @@ console.log(data);
 
 // fiter :kisi specific condition par array se data fetch karta hai and data return karta hai
 // data = emp.filter((e) => { return e.age > 89 })
-let fdata = emp.filter((e) => { return e.name == "hiresh" })
+let fdata = emp.filter((e) => { return e.name == "Himanshu" })
 console.log(fdata);
 
--- setintervel.js
+++ setintervel.js
@@ -1,10 +1,16 @@
+//setintervel is a function used to repeatedly execute a block of code at specified time intervels(in millisecond) it continues to call the function 
+//untill it is explicitly stoped using clearIntervel
 // two arguments accept karta hai pahle callback function and second is timer
 // let intervel = setInterval(() => { document.write("rinkiya ke papa") }, 1000)
 let intervel = setInterval(() => { alert("rinkiya ke papa") }, 1000)
-
+// to stop the repeated execution, you need to use clearinterval
 clearInterval(intervel)
 
 function start() {
+    let c = 0
     let count = document.getElementById('count')
-    st = setInterval(())
+    st = setInterval(() => { count.innerHTML = c++ }, 1000)
+}
+function stop() {
+    clearInterval(st)
 }
