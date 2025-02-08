7df560c8f6eb9afa00c9e5e1b3d692ec588b29f3
PranavMishra07 <pranav072003@gmail.com>
2025-02-08 15:07:03 +0530

    ddd

--- formonlocal.js
+++ formonlocal.js
@@ -7,5 +7,21 @@ function sub() {
     localStorage.setItem("address", address)
     localStorage.setItem("contact", contact)
     localStorage.setItem("city", city)
+    let firstname = localStorage.getItem("name")
+    let firstAddress = localStorage.getItem("address")
+    let firstContact = localStorage.getItem("contact")
+    let firstCity = localStorage.getItem("city")
+
+    let N = document.querySelector("#nn").innerHTML = firstname
+    let A = document.querySelector("#AA").innerHTML = firstAddress
+    let Co = document.querySelector("#Co").innerHTML = firstContact
+    let Ci = document.querySelector("#Ci").innerHTML = firstCity
+    let frm = document.querySelector("#frm").style.display = "none"
+    let h1 = document.querySelector(".head").style.display = "block"
     return false
+}
+function logout() {
+    localStorage.clear()
+    let h1 = document.querySelector(".head").style.display = "none"
+    frm.style.display = "block"
 }
