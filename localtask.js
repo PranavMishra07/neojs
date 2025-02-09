1658e5a7e6317fd2c8f64a61e771bc8d7d236810
PranavMishra07 <pranav072003@gmail.com>
2025-02-09 15:44:45 +0530

    dd

--- localtask.js
+++ localtask.js
@@ -1,17 +1,40 @@
-function submit() {
+function savedata() {
     let name = document.querySelector("#name").value
+    let age = document.querySelector("#age").value
+    let course = document.querySelector("#course").value
+    let ID = document.querySelector("#id").value
+    let contact = document.querySelector("#contact").value
+    let address = document.querySelector("#address").value
     localStorage.setItem("name", name)
+    localStorage.setItem("age", age)
+    localStorage.setItem("course", course)
+    localStorage.setItem("ID", ID)
+    localStorage.setItem("contact", contact)
+    localStorage.setItem("address", address)
     return false
 }
 function showdata() {
     let shname = document.querySelector("#shname")
     let shage = document.querySelector("#shage")
+    let shcourse = document.querySelector("#cc")
+    let shId = document.querySelector("#Ci")
+    let shCo = document.querySelector("#Co")
+    let shAA = document.querySelector("#AA")
 
     let getname = localStorage.getItem("name")
     let getage = localStorage.getItem("age")
+    let getcourse = localStorage.getItem("course")
+    let getID = localStorage.getItem("ID")
+    let getcontact = localStorage.getItem("contact")
+    let getaddress = localStorage.getItem("address")
 
     shname.innerHTML = getname
     shage.innerHTML = getage
+    shcourse.innerHTML = getcourse
+    shId.innerHTML = getID
+    shCo.innerHTML = getcontact
+    shAA.innerHTML = getaddress
+    return false
 }
 function clearr() {
     localStorage.clear()
