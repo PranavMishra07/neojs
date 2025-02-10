28a4411ca0f1a08b0fa8ca657f5f5db7c9e47f95
PranavMishra07 <pranav072003@gmail.com>
2025-02-10 18:57:43 +0530

    aa

--- jsonstring.js
+++ jsonstring.js
@@ -8,3 +8,17 @@ const multi = () => {
     localStorage.setItem("userdata", JSON.stringify(userdata))
     //json.stringify is use to change any  data into string
 }
+//json.parse is used to change string data to data which we set in setitem
+let data = JSON.parse(localStorage.getItem("userdata"))
+console.log(data);
+
+function loginform() {
+    let logindata = {
+        loginemail: document.querySelector("#loginemail").value,
+        loginpassword: document.querySelector("#loginpass").value
+    }
+    if (data.email != logindata.loginemail || data.password != logindata.loginpassword) {
+        alert("User Not Found")
+        return false
+    }
+}
