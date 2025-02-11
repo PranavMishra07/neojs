04d1e022684514c65ce2041c6e9587b6cf29c108
PranavMishra07 <pranav072003@gmail.com>
2025-02-11 15:15:37 +0530

    dd

--- jsonstring.js
+++ jsonstring.js
@@ -3,7 +3,31 @@ const multi = () => {
         name: document.querySelector("#name").value,
         email: document.querySelector("#email").value,
         password: document.querySelector("#password").value
-
+    }
+    if (userdata.name == "") {
+        alert("Please Enter Your Name")
+        document.querySelector("#name").focus()
+        return false
+    }
+    else if (userdata.email == "") {
+        alert("Please Enter Your Email")
+        document.querySelector("#email").focus()
+        return false
+    }
+    else if ((!userdata.email.includes("@"))) {     // includes check character is present or not in the input
+        alert("use @ for Validation")
+        document.querySelector("#email").focus()
+        return false
+    }
+    else if (userdata.password == "") {
+        alert("Please Enter Your password")
+        document.querySelector("#password").focus()
+        return false
+    }
+    else if (!(userdata.password.match(/[~!@#$%^&*>?}|]/))) {
+        alert("Please use some special character")
+        document.querySelector("#password").focus()
+        return false
     }
     localStorage.setItem("userdata", JSON.stringify(userdata))
     //json.stringify is use to change any  data into string
@@ -19,6 +43,8 @@ function loginform() {
     }
     if (data.email != logindata.loginemail || data.password != logindata.loginpassword) {
         alert("User Not Found")
+        document.querySelector("#loginemail").focus()
         return false
     }
+
 }
