655c3b038ec14f862d1a17acdaba071cc6b9bf2d
PranavMishra07 <pranav072003@gmail.com>
2025-02-15 14:41:45 +0530

    ii

--- apifetch.js
+++ apifetch.js
@@ -1,3 +1,6 @@
+// async is used to wait for the data to be fetched from the api and then show the data in the browser 
+// fetch is used to fetch the data from the api
+await is used to wait for the data to be fetched from the api
 async function Demo() {
     let apidata = await fetch("https://jsonplaceholder.typicode.com/comments")  //await is used to wait for the all code is 
     let data = await apidata.json()   // json() is used to convert the data into json format
